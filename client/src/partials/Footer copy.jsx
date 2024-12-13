e653312de0809242722e6a66e736375c4c3c3db3
echeng6822 <echeng6822@gmail.com>
2024-12-12 22:32:36 -0500

    Temp Nav and footer

---
+++ client/src/partials/Footer copy.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div>
+      <h4>Quick Links</h4>
+      <ul>
+        <li>Facilities</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Explore</h4>
+      <ul>
+        <li>News</li>
+        <li>Events</li>
+        <li>Careers</li>
+      </ul>
+    </div>
+    <p>© McGill University 2024</p>
+  </footer>
+);
+
+export default Footer;
