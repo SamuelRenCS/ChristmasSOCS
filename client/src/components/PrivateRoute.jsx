e2a7cb49ceeaf5329987aa9d754f224ad310931a
Samuel Ren <samuelrenonly@gmail.com>
2024-12-16 17:34:11 -0500

    location redirect after login

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import React from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const PrivateRoute = () => {
-    const { isAuthenticated } = useAuth();
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
 
-    // if not authenticated, redirect to login
-    // otherwise, render the child routes
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  // if not authenticated, redirect to login
+  // otherwise, render the child routes
+  return isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 };
 
 export default PrivateRoute;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,90 +1,87 @@
-import React, { useState } from 'react';
-import { login } from '../api/api.js'; // import the login function from api.js
+import React, { useState } from "react";
+import { login } from "../api/api.js"; // import the login function from api.js
 import "../styles/Login.css";
-import Input from '../components/Input';
-import Button from '../components/Button';
-import { useAuth } from '../context/AuthContext'
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate, useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Login() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { login: authLogin } = useAuth();
 
-    const navigate = useNavigate();
-    const { login: authLogin } = useAuth();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setFormData((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await login(formData);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+      const token = response.data.data.token;
 
-        try {
-            const response = await login(formData);
-            
-            const token = response.data.data.token;
-            
-            if (!token) {
-                toast.error('Login failed: No authentication token');
-                return;
-            }
-    
-            authLogin(token);  // use the login function from AuthContext
-    
-            toast.success('Login successful');
-    
-            // redirect to dashboard after successful login with some delay
-            setTimeout(() => navigate('/dashboard'), 200);
-    
-        } catch (error) {
-            const errorMessage = error.response?.data?.message 
-                || error.message 
-                || 'Login failed';
-            
-            toast.error(errorMessage);
-        }
+      if (!token) {
+        toast.error("Login failed: No authentication token");
+        return;
+      }
+
+      authLogin(token); // use the login function from AuthContext
+
+      toast.success("Login successful");
+
+      // redirect to dashboard after successful login with some delay
+      const origin = location.state?.from?.pathname || "/dashboard";
+
+      setTimeout(() => navigate(origin), 200);
+    } catch (error) {
+      const errorMessage =
+        error.response?.data?.message || error.message || "Login failed";
+
+      toast.error(errorMessage);
     }
+  };
 
-    return (
-    
-            <div className="login-container">
-                <div className="login-box">
-                    <h2>Login</h2>
-                    <form onSubmit={handleSubmit} className="login-form">
-                        <Input
-                            label="Email"
-                            type="email"
-                            name="email"
-                            placeholder="Enter email"
-                            formType="login"
-                            onChange={handleChange}
-                        />
-                        <Input  
-                            label="Password"
-                            type="password"
-                            name="password"
-                            placeholder="Enter password"
-                            formType="login"
-                            onChange={handleChange}
-                        />
-                        <Button type="submit" text="Login" />
-                    </form>
-                    </div>
-                    {/* <div className="image-box">
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit} className="login-form">
+          <Input
+            label="Email"
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            formType="login"
+            onChange={handleChange}
+          />
+          <Input
+            label="Password"
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            formType="login"
+            onChange={handleChange}
+          />
+          <Button type="submit" text="Login" />
+        </form>
+      </div>
+      {/* <div className="image-box">
                       <img src={mcgillImage} alt="McGill Campus" />
                     </div> */}
-                </div>
-    
-    );
+    </div>
+  );
 }
 
 export default Login;
