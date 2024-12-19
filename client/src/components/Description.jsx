5c68cb0a5b2e4532725d3bd1437df738ddffdcaa
shitar <102865806+shitar@users.noreply.github.com>
2024-12-19 16:48:55 -0500

    description responsiveness

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "./Description.module.css"; // Import the CSS module
 
 const Description = ({ image, title, text, reverse = false, display, onClick}) => {
+  const flexDirection = window.innerWidth <= 768 ? "column" : reverse ? "row-reverse" : "row";
+
   return (
-    <div className={`${styles.descriptionSection} ${reverse ? styles.reverseOrder : ""}`}>
+    <div className={`${styles.descriptionSection} ${reverse ? styles.reverseOrder : ""}`} style={{ flexDirection }}>
       {/* Left Side - Image */}
       <div className={styles.descriptionImage}>
         <img src={image} alt="Team projects overview" />
